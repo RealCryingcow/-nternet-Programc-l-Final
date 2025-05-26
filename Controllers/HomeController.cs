fe5dce21063058bceab6a18d72203c2de7106925
Mahir Tekin <mahir_tekin998@hotmail.com>
2025-05-26 20:49:03 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyPortalUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Login() => View();
+        public IActionResult Register() => View();
+        public IActionResult Profile() => View();
+
+        public IActionResult Surveys() => View();
+        public IActionResult MySurveys() => View();
+        public IActionResult AllSurveys() => View();
+
+        public IActionResult Questions(int surveyId) => View();
+        public IActionResult SurveyResponses(int surveyId) => View();
+        public IActionResult MyResponses() => View();
+
+        public IActionResult Users() => View();
+        public IActionResult UserDetail(int id) => View();
+        public IActionResult Roles(int userId) => View();
+    }
+}
